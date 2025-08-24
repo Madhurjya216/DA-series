538b50bdadb4b37d837e42bc67fc534c6175e7d9
madhurjya kashyap <madhurjyakashyap216@gmail.com>
2025-08-24 11:56:51 -0500

    csv & json - UI

---
+++ mathplotlib/practice.py
@@ -0,0 +1,40 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+
+# x = [1, 2, 3, 4, 5] 
+# y = [2, 4, 6, 8, 10]
+
+# plt.plot(x, y, color='green', marker='^', linestyle='dashed')
+# plt.title("Practice Styling")
+# plt.grid(True)
+# plt.show()
+
+
+
+# ================== csv data =================== #
+# df = pd.read_csv('D:\Workspace\PYTHON\Data Analytics\mathplotlib\orders.csv')
+
+# plt.plot(df['OrderDate'], df['SalesAmount'], label='Sales Amount')
+# plt.xlabel("Order Date")
+# plt.ylabel("Sales Amount")
+# plt.title("Sales Amount by Order Date")
+# plt.grid(True)
+# plt.legend()
+# plt.show()
+
+
+
+# =================== json data =================== #
+# df = pd.read_json('D:\Workspace\PYTHON\Data Analytics\mathplotlib\data.json')
+# # data = pd.read_json(df)
+# # name = data['name']
+# # age = data['age']
+# # city = data['city']
+
+# plt.plot(df['name'], df['age'], linestyle='solid', marker='o', color='red')
+# plt.grid(True)
+# plt.show()
+
+# print(df)
